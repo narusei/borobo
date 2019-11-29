071ae10ba6ddc6e7ea88609b975cc52fb3b463a6
masaki obayashi <tusanch6@gmail.com>
2019-11-29 23:39:19 +0900

    feature(quest-search):クエスト検索ページのUIを作成した

---
+++ app/javascript/components/quest-search/BQuestSearchContainer/BQuestSearchContainer.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="search_container">
+      <div class="cp_iptxt">
+        <label class="ef">
+          <input type="text" placeholder="タイトルを入力" v-model="search_word_title" />
+        </label>
+      </div>
+      <div class="cp_iptxt">
+        <label class="ef">
+          <input type="text" placeholder="タグを入力" v-model="search_word_tags" />
+        </label>
+      </div>
+      <div class="cp_iptxt">
+        <label class="ef">
+          <input type="text" placeholder="報酬を入力" v-model="search_word_title" />
+        </label>
+      </div>
+      <section>
+        <b-button @click="quest_search" class="quest_search_button">検索</b-button>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class NAME extends Vue {
+  // 1.@Prop
+  // 2.property
+  // 3.getter
+  // 4.@Watch
+  // 5.method
+
+  search_word_title: string = "";
+  search_word_tags: [];
+  search_word_reword: string = "";
+}
+</script>
+<style lang="scss" scoped>
+
+//search word (title,tag,reword)
+.cp_iptxt {
+  position: relative;
+  margin: 10px;
+}
+
+.cp_iptxt input[type="text"] {
+  padding: 8px 8px 8px 8px;
+  box-sizing: border-box;
+  width: 100%;
+  transition: 0.3s;
+  letter-spacing: 1px;
+  color: #000000;
+  border: 1px solid #bbbbbb;
+  border-radius: 4px;
+  font-size: 18px;
+}
+.ef input[type="text"]:focus {
+  outline: none;
+}
+
+.quest_search_button {
+  width: 20%;
+  margin-left: 78%;
+  margin-top: 0px;
+  margin-bottom: 10px;
+  font-size: 15px;
+  border: 1px solid #51e898;
+  box-shadow: 1px 1px 1px 1px #bbbbbb;
+}
+</style>
---
+++ app/javascript/components/quest-search/BQuestSearchPage/BQuestSearchPage.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="back_button" @click="back()">
+        <b-icon icon="chevron-left" size="is-medium"></b-icon>
+      </div>
+      <div class="PageTitle">検索</div>
+    </div>
+    <BQuestSearchContainer class="TopBar" />
+    <ul class="tab">
+      <div class="hito" @click="setTab(0)">
+        ヒト
+        <div id="chose" v-if="page_tab_num==0"></div>
+      </div>
+      <div class="mono" @click="setTab(1)">
+        モノ
+        <div id="chose" v-if="page_tab_num==1"></div>
+      </div>
+    </ul>
+    <div class="quest_box">
+      <BQuestList v-bind:tab_num="page_tab_num" :questlist="questlist"></BQuestList>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+import BQuestList from "@/components/quest-search/BQuestList/BQuestList.vue";
+import BQuestSearchContainer from "@/components/quest-search/BQuestSearchContainer/BQuestSearchContainer.vue";
+
+@Component({
+  components: {
+    BQuestSearchContainer,
+    BQuestList
+  }
+})
+export default class BQuestSearchPage extends Vue {
+  // 1.@Prop()
+  // 2.property
+  // 3.getter
+  // 4.@Watch
+  // 5.method
+
+  page_tab_num: number = 0;
+
+  quest_search() {
+    //alert();
+  }
+
+  setTab(num: number) {
+    this.page_tab_num = num;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  height: 60px;
+  background-color: #51e898;
+
+  padding-top: 12px;
+  padding-bottom: 9px;
+  padding-left: 12px;
+  padding-right: 12px;
+
+  display: flex;
+
+  .back_button {
+    height: 46px;
+    width: 40px;
+  }
+  .PageTitle {
+    font-size: 20px;
+    margin-top: 1px;
+  }
+}
+
+//tab hito mono
+ul.tab {
+  .hito {
+    /* Text/Regular 14px */
+    font-size: 20px;
+    width: 50%;
+    float: left;
+    text-align: center;
+    padding-top: 1px;
+    padding-bottom: 1px;
+
+    #chose {
+      border-bottom: solid 2px #51e898;
+    }
+  }
+  .mono {
+    font-size: 20px;
+    width: 50%;
+    float: right;
+    text-align: center;
+    padding-top: 1px;
+    padding-bottom: 1px;
+
+    #chose {
+      border-bottom: solid 2px #51e898;
+    }
+  }
+}
+
+.quest_box {
+  clear: both;
+}
+
+</style>
