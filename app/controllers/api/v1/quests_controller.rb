7a72447105d422eab6fa0b44d4fedc6e69016d34
kueika <nakamuranakatu@yahoo.co.jp>
2019-12-24 16:35:17 +0900

    feautre(quests): クエストのAPI(検索部分)を作りました

--- app/controllers/api/v1/quests_controller.rb
+++ app/controllers/api/v1/quests_controller.rb
@@ -3,8 +3,33 @@ class Api::V1::QuestsController < ApplicationController
     # before_action :set_quest
     # GET /Quests
     def index
-        @quests = Quest.all
-        json_response(@quests)
+        latest_qid = params[:latest_qid] if params[:latest_qid].present?
+        tags_exist =  params[:search_tags].present?
+        reward_exist =  params[:search_reward].present?
+        author_exist =  params[:search_uname].present?
+        quest_tag = params[:search_tags]
+        quest_reward = params[:search_reward]
+        quest_author = params[:search_uname]
+        @quests = Quest.limit(8).offset(latest_qid) if params[:latest_qid].present?
+        if tags_exist && reward_exist && author_exist
+            @quests = Quest.where('tag LIKE ?', "%#{quest_tag}%").where('reward LIKE ?',"%#{quest_reward}%").where(author:params[:search_uname])  
+        elsif  tags_exist && reward_exist
+            @quests = Quest.where('tag LIKE ?', "%#{quest_tag}%").where('reward LIKE ?',"%#{quest_reward}%")
+        elsif  tags_exist && author_exist
+            @quests = Quest.where('tag LIKE ?', "%#{quest_tag}%").where(author:params[:search_uname])
+        elsif  author_exist && reward_exist
+            @quests = Quest.where(author:params[:search_uname]).where('reward LIKE ?',"%#{quest_reward}%")
+        elsif tags_exist
+            @quests = Quest.where('tag LIKE ?', "%#{quest_tag}%")
+        elsif reward_exist
+            @quests = Quest.where('reward LIKE ?',"%#{quest_reward}%")
+        elsif author_exist
+            @quests = Quest.where(author:params[:search_uname])
+        else
+            @quests = Quest.limit(8).offset(0)
+        end
+        json_response(@quests) 
+        
     end
     # POST /Quests
     def create
