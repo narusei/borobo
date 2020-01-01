0e82c248927b3265e6c5c5e0a306115fd0f19a3f
narusei <naruse1wtbdev@gmail.com>
2020-01-01 19:05:07 +0900

    feat(user-create): ユーザ新規作成機能を作りました

--- app/javascript/api/quest/index.ts
+++ app/javascript/api/quest/index.ts
@@ -1,6 +1,12 @@
 import http from "@/utils/requests";
 
 export default {
+  createUser(param: any) {
+    return http.post("http://localhost:8000/api/v1/users_info", {
+      params: param
+    });
+  },
+
   updateUser(param: any, user_id: number) {
     return http.put(
       "http://localhost:8000/api/v1/users/" + user_id + "/users_info",
---
+++ app/javascript/components/user-create/BUserCreatePage/BUserCreatePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-user-edit-top-bar pageName="ユーザ情報登録" />
+    <b-user-edit-contents @editUser="createUser($event)" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch, Emit } from "vue-property-decorator";
+import BUserEditTopBar from "@/components/user-edit/BUserEditTopBar";
+import BUserEditContents from "@/components/user-edit/BUserEditContents";
+import { UserItem } from "@/models/user/UserItem";
+import { UserInfo } from "@/models/user/UserInfo";
+
+@Component({
+  components: {
+    BUserEditTopBar,
+    BUserEditContents
+  }
+})
+export default class BUserCreatePage extends Vue {
+  // 1.@Prop
+  // 2.property
+  // 3.getter
+  // 4.@Watch
+  // 5.method
+  @Emit("createUser")
+  createUser(param: UserInfo) {
+    return param;
+  }
+}
+</script>
