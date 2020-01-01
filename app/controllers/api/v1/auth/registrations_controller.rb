520366ee559f958b0dda05026aaf79b6f9c4698a
kueika <nakamuranakatu@yahoo.co.jp>
2020-01-02 01:15:17 +0900

    edit registration_controller routes.rb config

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,16 @@
-class Api::V1::Auth::RegistrationsController < ApplicationController
+module Api
+    module V1
+      module Auth
+        class RegistrationsController < DeviseTokenAuth::RegistrationsController
+          private
+          def sign_up_params
+            params.permit(:nickname, :email, :password, :password_confirmation)
+          end
+  
+          def account_update_params
+            params.permit(:nickname, :email)
+          end
+        end
+      end
+    end
 end
